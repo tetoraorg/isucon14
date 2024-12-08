d4cf5c7db86c84a280ded8ce582f541e9fe0d41b
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2024-12-08 14:24:31 +0900

    chair_idに対するindexを追加

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -46,7 +46,8 @@ CREATE TABLE chair_locations
   latitude   INTEGER     NOT NULL COMMENT '経度',
   longitude  INTEGER     NOT NULL COMMENT '緯度',
   created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  INDEX idx_chair_id (chair_id)
 )
   COMMENT = '椅子の現在位置情報テーブル';
 
