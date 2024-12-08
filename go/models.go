a122a760eb05bd684842ce2764d71e90e9e3b11f
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2024-12-08 12:04:09 +0900

    chair

--- go/models.go
+++ go/models.go
@@ -6,14 +6,16 @@ import (
 )
 
 type Chair struct {
-	ID          string    `db:"id"`
-	OwnerID     string    `db:"owner_id"`
-	Name        string    `db:"name"`
-	Model       string    `db:"model"`
-	IsActive    bool      `db:"is_active"`
-	AccessToken string    `db:"access_token"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	ID                     string       `db:"id"`
+	OwnerID                string       `db:"owner_id"`
+	Name                   string       `db:"name"`
+	Model                  string       `db:"model"`
+	IsActive               bool         `db:"is_active"`
+	AccessToken            string       `db:"access_token"`
+	CreatedAt              time.Time    `db:"created_at"`
+	UpdatedAt              time.Time    `db:"updated_at"`
+	TotalDistance          int          `db:"total_distance"`
+	TotalDistanceUpdatedAt sql.NullTime `db:"total_distance_updated_at"`
 }
 
 type ChairModel struct {
