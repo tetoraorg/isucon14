8f3d6872413a99a2b4b015cd34d34ade0ac25ca5
tesso <48358052+tesso57@users.noreply.github.com>
2024-12-08 11:36:59 +0900

    Get /api/internal/matching (#3)
    * impl
    
    * fix
    
    * debug
    
    * fix
    
    * f
    
    * fix
    
    * fix
    
    * cmd
    
    * f
    
    * log level
    
    * serivce
    
    * f
    
    ---------
    
    Co-authored-by: isucon <isucon@isucon>

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"errors"
-	"net/http"
+	"log/slog"
 )
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
-func internalGetMatching(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+func internalGetMatching(ctx context.Context) {
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
 	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			w.WriteHeader(http.StatusNoContent)
+			slog.Info("no rides")
 			return
 		}
-		writeError(w, http.StatusInternalServerError, err)
+		slog.Error("Failed to fetch ride", err)
 		return
 	}
 
@@ -25,14 +25,13 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	for i := 0; i < 10; i++ {
 		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
 				return
 			}
-			writeError(w, http.StatusInternalServerError, err)
+			slog.Error("Failed to fetch chair", err)
 		}
 
 		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
+			slog.Error("Failed to fetch chair", err)
 			return
 		}
 		if empty {
@@ -40,14 +39,12 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if !empty {
-		w.WriteHeader(http.StatusNoContent)
 		return
 	}
 
 	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
-		writeError(w, http.StatusInternalServerError, err)
+		slog.Error("Failed to update ride", err)
 		return
 	}
 
-	w.WriteHeader(http.StatusNoContent)
 }
