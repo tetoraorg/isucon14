5b5b5666f894ab3a57e94732d381ae59235a4f3c
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2024-12-08 11:30:24 +0900

    距離を事前計算してみる

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -30,8 +30,10 @@ CREATE TABLE chairs
   model        TEXT         NOT NULL COMMENT '椅子のモデル',
   is_active    TINYINT(1)   NOT NULL COMMENT '配椅子受付中かどうか',
   access_token VARCHAR(255) NOT NULL COMMENT 'アクセストークン',
+  total_distance INTEGER    NOT NULL DEFAULT 0 COMMENT '総移動距離',
   created_at   DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '登録日時',
   updated_at   DATETIME(6)  NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新日時',
+  total_distance_updated_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '総移動距離更新日時',
   PRIMARY KEY (id),
   INDEX idx_access_token (access_token),
   INDEX idx_owner_id (owner_id)
