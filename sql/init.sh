ccc46f4a3f05a3c58f027dddf3595f27f945cd8a
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2024-12-08 11:56:02 +0900

    mysql

--- sql/init.sh
+++ sql/init.sh
@@ -36,7 +36,7 @@ gzip -dkc 3-initial-data.sql.gz | mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME"
 
--u"$ISUCON_DB_USER" \
+mysql -u"$ISUCON_DB_USER" \
 		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
