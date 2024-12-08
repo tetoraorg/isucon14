09250b816679e9294144696a475ff748168f46ec
ras0q <ras0q@users.noreply.github.com>
2024-12-08 14:35:53 +0900

    マッチングを愚直にgoで書いた

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -2,46 +2,72 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"errors"
 	"log/slog"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 func internalGetMatching(ctx context.Context) {
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
-	ride := &Ride{}
-	if err := database().GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return
-		}
-		slog.Error("Failed to fetch ride", err)
+	var chairs []*Chair
+	if err := database().SelectContext(ctx, &chairs, "SELECT * FROM chairs WHERE is_active = TRUE"); err != nil {
+		slog.Error("Failed to fetch chairs", err)
 		return
 	}
 
-	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := database().GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				return
-			}
-			slog.Error("Failed to fetch chair", err)
+	chairIDs := make([]string, 0, len(chairs))
+	for _, chair := range chairs {
+		chairIDs = append(chairIDs, chair.ID)
+	}
+
+	var rides []*Ride
+	query, params, err := sqlx.In("SELECT * FROM rides WHERE chair_id IN (?)", chairIDs)
+	if err != nil {
+		slog.Error("Failed to parse rides in query", err)
+		return
+	}
+	if err := database().SelectContext(ctx, &rides, database().Rebind(query), params...); err != nil {
+		slog.Error("Failed to fetch rides", err)
+		return
+	}
+
+	rideIDs := make([]string, 0, len(rides))
+	for _, ride := range rides {
+		rideIDs = append(rideIDs, ride.ID)
+	}
+
+	var rideStatuses []*RideStatus
+	query, params, err = sqlx.In("SELECT * FROM ride_statuses WHERE ride_id IN (?)", rideIDs)
+	if err != nil {
+		slog.Error("Failed to parse ride_statuses in query", err)
+		return
+	}
+	if err := database().SelectContext(ctx, &rideStatuses, database().Rebind(query), params...); err != nil {
+		slog.Error("Failed to fetch ride_statuses", err)
+		return
+	}
+
+	var matchedRide *Ride
+	for _, ride := range rides {
+		if len(rideStatuses) == 0 {
+			matchedRide = ride
+			break
 		}
 
-		if err := database().GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			slog.Error("Failed to fetch chair", err)
-			return
+		var count int
+		for _, rideStatus := range rideStatuses {
+			if rideStatus.RideID == ride.ID {
+				count++
+			}
 		}
-		if empty {
+
+		if count < 6 {
+			matchedRide = ride
 			break
 		}
 	}
-	if !empty {
-		return
-	}
 
-	if _, err := database().ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	if _, err := database().ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matchedRide.ChairID, matchedRide.ID); err != nil {
 		slog.Error("Failed to update ride", err)
 		return
 	}
