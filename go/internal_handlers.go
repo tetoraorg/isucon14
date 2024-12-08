a6b713bfc8f3d6dfb54f17e69c5ec8ed3df5c4fa
isucon <isucon@isucon>
2024-12-08 12:20:29 +0900

    もどす

--- go/internal_handlers.go
+++ go/internal_handlers.go
@@ -11,57 +11,39 @@ import (
 func internalGetMatching(ctx context.Context) {
 	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
-	if err := db.GetContext(ctx, ride, `
-	SELECT * 
-	FROM rides
-	WHERE chair_id IS NULL
-	ORDER BY created_at
-	LIMIT 1
-`); err != nil {
+	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			slog.Info("no rides")
 			return
 		}
-		slog.Error("Failed to fetch ride", err)
+		slog.Error("error response wrote", err)
 		return
 	}
 
 	matched := &Chair{}
-	// 「空いている椅子」を一度のクエリで取得する例
-	if err := db.GetContext(ctx, matched, `
-	SELECT c.*
-	FROM chairs c
-	WHERE c.is_active = TRUE
-	  AND NOT EXISTS (
-	    SELECT 1
-	    FROM rides r
-	    JOIN ride_statuses rs ON r.id = rs.ride_id
-	    WHERE r.chair_id = c.id
-	    GROUP BY r.id
-	    HAVING COUNT(rs.chair_sent_at) <> 6
-	  )
-	ORDER BY RAND()
-	LIMIT 1
-`); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			// 空いている椅子が無い場合はマッチングできない
-			slog.Info("no available chairs")
+	empty := false
+	for i := 0; i < 10; i++ {
+		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				return
+			}
+			slog.Error("error response wrote", err)
+		}
+
+		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+			slog.Error("error response wrote", err)
 			return
 		}
-		slog.Error("Failed to fetch empty chair", err)
+		if empty {
+			break
+		}
+	}
+	if !empty {
 		return
 	}
 
-	// 椅子が取得できたのでライドに割り当て
-	if _, err := db.ExecContext(ctx, `
-	UPDATE rides
-	SET chair_id = ?
-	WHERE id = ?
-`, matched.ID, ride.ID); err != nil {
-		slog.Error("Failed to update ride", err)
+	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+		slog.Error("error response wrote", err)
 		return
 	}
-
-	slog.Info("Successfully matched ride with chair", slog.Any("ride_id", ride.ID), slog.Any("chair_id", matched.ID))
 
 }
