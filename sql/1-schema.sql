e794d0fed0e578417a7a5866312a991acfe20ef2
ras0q <ras0q@users.noreply.github.com>
2024-12-10 00:02:39 +0900

    chair_idのidxはる

--- sql/1-schema.sql
+++ sql/1-schema.sql
@@ -95,6 +95,7 @@ CREATE TABLE rides
   created_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '要求日時',
   updated_at            DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '状態更新日時',
   PRIMARY KEY (id),
+  INDEX idx_chair_id (chair_id),
   INDEX idx_user_id_created_at (user_id, created_at DESC),
   INDEX idx_chair_id_updated_at (chair_id, updated_at DESC)
 )
